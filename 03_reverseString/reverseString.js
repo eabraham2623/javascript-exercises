b9f7b667ce4ad8cd5277c366b8571451ac35e03b
Emil A <eabraham2623@gmail.com>
2022-05-07 15:30:20 -0400

    Completed first 3 exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let strLength = str.length;
+    let result = "";
+    // start iteration from end of string 
+    // and keep going until 0th index of string
+    for (i = strLength - 1; i != -1 ; i--)
+    {
+        result+=str[i];
+    }
+    return result;
 };
 
 // Do not edit below this line
