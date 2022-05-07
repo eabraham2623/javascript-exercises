012d5d8281d9e4f181b75df49957f79a92f25fbf
Emil A <eabraham2623@gmail.com>
2022-05-07 18:26:51 -0400

    Adding solution for 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
+    // check to see if first or last numbers are negative or not numbers
+    if (first < 0 || typeof first !== 'number' || last < 0 || typeof last !== 'number')
+    {
+        return "ERROR";
+    }
+    let numOfIntegers = Math.abs(first - last) + 1;
 
+    // summation formula used
+    return (numOfIntegers * (first + last))/2;  
 };
 
 // Do not edit below this line
