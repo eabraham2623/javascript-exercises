4bfb187e07165d3ccb8c5409fc97579eb32e96a9
Emil A <eabraham2623@gmail.com>
2022-05-07 16:22:57 -0400

    Passed first test 04 exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,27 @@
-const removeFromArray = function() {
 
+
+const removeFromArray = function(arr, ...args) {
+    let result = arr.filter (
+        function(el) {
+            return el != args;
+        }
+    );
+    return result;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+/*
+
+[1, 2, 3],  3
+
+function takes ...arg as input
+
+filter if element is equal to 3
+function(el)
+{
+    return 3 == el;
+}
+*/
