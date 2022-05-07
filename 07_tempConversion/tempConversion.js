aa4202fcf0f6f9528911d3f14edb7d3211cebc3f
Emil A <eabraham2623@gmail.com>
2022-05-07 19:58:30 -0400

    adding solution 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,25 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  // perform farenheit to celsisus calculation
+  let convertedTemperature = ((temp - 32) * (5/9))//.toFixed(1);
+  if (!(convertedTemperature % 1 == 0))
+  {
+    // return value fixed to 1 decimal place if not divisible by 1
+    // convert to number, unary operator used
+    return +(convertedTemperature.toFixed(1));
+  }
+  return convertedTemperature;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  // perform celsisus to farenheit calculation
+  let convertedTemperature =  ((temp) * (9/5) + 32);
+  if (!(convertedTemperature % 1 == 0))
+  {
+    // return value fixed to 1 decimal place if not divisible by 1
+    // convert to number, unary operator used
+    return +(convertedTemperature.toFixed(1));
+  }
+  return convertedTemperature;
 };
 
 // Do not edit below this line
