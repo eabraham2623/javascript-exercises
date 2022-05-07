d27833f0e0e347d42ae93483200c634f48dea1d1
Emil A <eabraham2623@gmail.com>
2022-05-07 18:49:32 -0400

    Adding helper functions

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,31 @@
-const leapYears = function() {
+function isDivisibleByFour(year)
+{
+    return ((year % 4) == 0);
+    
+}
 
+function isDivisibleByOneHundred(year)
+{
+    return ((year % 100) == 0);
+}
+
+function isDivisibleByFourHundred(year)
+{
+    return ((year % 400) == 0);
+}
+
+function isSpecialCaseLeapYear(year)
+{
+    return !(isDivisibleByOneHundred(year)) || isDivisibleByFourHundred(year);
+}
+function isLeapYear(year)
+{
+    return (isDivisibleByFour(year) && isSpecialCaseLeapYear(year));
+}
+
+const leapYears = function(year) 
+{
+    return isLeapYear(year);
 };
 
 // Do not edit below this line
