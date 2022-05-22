2916d12073626260ae9dbb0e1035a83563184024
Emil A <eabraham2623@gmail.com>
2022-05-22 12:14:17 -0400

    Added calculator solutions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,55 @@
-const add = function() {
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a,b) {
+	return a - b;
+};
+
+const sum = function(arr) {
+  
+  let currentTotal = 0;
+  for (let i = 0; i < arr.length; i++)
+  {
+    currentTotal = add(currentTotal, arr[i]);
+  }
+  return currentTotal;
+};
+
+const multiply = function(arr) {
+  let currentProduct = 1;
+  for (let i = 0; i < arr.length; i++)
+  {
+    currentProduct *= arr[i];
+  }
+  return currentProduct;
+  
+};
+
+const power = function(base, exponent) {
+  if (exponent == 0)
+  {
+    return 1;
+  }
+  let currentProduct = 1;
+  for (let i = 0; i < exponent; i++)
+  {
+    currentProduct *= base;
+  }
+  return currentProduct;
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) 
+{
+  if (num == 0)
+  {
+    return 1;
+  }
+  else
+  {
+    return num * factorial(num - 1) ;
+  }
 };
 
 // Do not edit below this line
