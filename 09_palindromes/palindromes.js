23ef5053cc50f4788c602110eb017331e2cc87e6
Emil A <eabraham2623@gmail.com>
2022-05-22 13:29:01 -0400

    Adding palindrome solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,29 @@
-const palindromes = function () {
+function reverseString(str)
+{
+    let reversed = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+    let lowerCasedReversed = reversed.toLowerCase();
+    let lowerCasedNoSpaceReversed = lowerCasedReversed.replace(/\s+/g, '');
+    let lowerCasedNoSpaceNoSpecialCharReversed = lowerCasedNoSpaceReversed.replace(/[^\w\s]/gi, '');
+    return lowerCasedNoSpaceNoSpecialCharReversed;
+}
 
+const palindromes = function (str) {
+    reversedString = reverseString(str);
+
+    let strLower = str.toLowerCase();
+    let strNoSpaceLower = strLower.replace(/\s+/g, '');
+    let strNoSpaceNoSpecialCharLower = strNoSpaceLower.replace(/[^\w\s]/gi, '');
+    if (strNoSpaceNoSpecialCharLower === reversedString)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 };
 
 // Do not edit below this line
